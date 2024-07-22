09c075e452240974602b5f5f8bfb11858e40625e
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-07-22 18:33:07 +0200

    update markup  is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
 import userData from "./data/userData.json";
 import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </>
   );
 };
